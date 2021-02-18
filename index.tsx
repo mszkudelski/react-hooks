24788f938c421e938346b7e811ef00e3f971ff6d
mszkudelski <marek.szkudelski@gmail.com>
2021-02-18 12:42:04 +0100

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
 
-interface AppProps { }
+interface AppProps {}
 interface AppState {
   name: string;
 }
 
 class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React'
-    };
-  }
-
   render() {
     return (
       <div>
         <Hello />
-        <p>
-        </p>
+        <p />
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
