006730eefb270cc4a2cc032eb89983756223856a
mszkudelski <marek.szkudelski@gmail.com>
2021-02-18 12:47:23 +0100

    1 file modified

--- useFetchData.ts
+++ useFetchData.ts
@@ -2,21 +2,11 @@ import { useEffect, useState } from "react";
 import { FetchDataResult } from "./fetch-data-results";
 
 export const useFetchData = <T>(
-  callback: () => Promise<T>,
-  deps: any[] = []
+  callback: () => Promise<T>
 ): FetchDataResult<T> => {
-  const [state, setState] = useState<FetchDataResult<T>>({
+  return {
     data: null,
     error: null,
     status: "loading"
-  });
-
-  useEffect(() => {
-    setState({ data: null, error: null, status: "loading" });
-    callback()
-      .then(data => setState({ data, error: null, status: "success" }))
-      .catch(error => setState({ data: null, error, status: "error" }));
-  }, [...deps]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  return state;
+  };
 };
