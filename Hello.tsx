0daa84b741b404e4acb8657b1a463f4ade513e16
mszkudelski <marek.szkudelski@gmail.com>
2021-03-02 14:05:45 +0100

    1 file modified

--- Hello.tsx
+++ Hello.tsx
@@ -3,6 +3,14 @@ import { useFetchData } from "./useFetchData";
 
 export default () => {
   const { status } = useFetchData(() => new Promise(r => setTimeout(r, 2000)));
+  const { status: error } = useFetchData(
+    () => new Promise((_, r) => setTimeout(r, 2000))
+  );
 
-  return <h1>Success callback: {status}!</h1>;
+  return (
+    <div>
+      <h1>Success callback: {status}!</h1>
+      <h1>Error callback: {error}!</h1>
+    </div>
+  );
 };
